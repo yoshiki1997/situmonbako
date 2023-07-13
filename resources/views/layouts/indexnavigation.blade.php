459a8c2faecb2dd4a0958109350eb5424ca33c2e
yoshiki1997 <yoshikiomayu@gmail.com>
2023-07-13 14:04:33 +0900

    indexnavgation

--- resources/views/layouts/indexnavigation.blade.php
+++ resources/views/layouts/indexnavigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="mt-2">
+{{--<nav class="mt-2">
     <ul class="flex justify-center space-x-4 overflow-x-auto">
         <li id="teratail-item" class="index-list cursor-pointer px-4 py-2 rounded-md">Teratail</li>
         <li id="youtube-item" class="index-list cursor-pointer px-4 py-2 rounded-md">YouTube</li>
@@ -8,4 +8,19 @@
         <li id="stack-overflow-ja-item" class="index-list cursor-pointer px-4 py-2 rounded-md bg-gray-200 text-gray-600">Stack Overflow (日本語)</li>
         <li id="ranking-item" class="index-list cursor-pointer px-4 py-2 rounded-md">Rankings</li>
     </ul>
+</nav>--}}
+<nav class="mt-2">
+    <div class="flex overflow-x-auto">
+        <ul class="flex space-x-4">
+            <li id="teratail-item" class="index-list cursor-pointer px-4 py-2 rounded-md">Teratail</li>
+            <li id="youtube-item" class="index-list cursor-pointer px-4 py-2 rounded-md">YouTube</li>
+            <li id="qitta-item" class="index-list cursor-pointer px-4 py-2 rounded-md">Qitta</li>
+            <li id="stack-overflow-item" class="index-list cursor-pointer px-4 py-2 rounded-md">Stack Overflow</li>
+            <li id="stack-exchange-gamedev-item" class="index-list cursor-pointer px-4 py-2 rounded-md">Stack Exchange GameDev</li>
+            <li id="stack-overflow-ja-item" class="index-list cursor-pointer px-4 py-2 rounded-md bg-gray-200 text-gray-600">Stack Overflow (日本語)</li>
+            <li id="ranking-item" class="index-list cursor-pointer px-4 py-2 rounded-md">Rankings</li>
+        </ul>
+    </div>
 </nav>
+
+
